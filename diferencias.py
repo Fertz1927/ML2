955c4a6b824e7d589cbd404a5d1c662c568227db
Fertz1927 <161084824+Fertz1927@users.noreply.github.com>
2026-09-12 13:16:02 -0500

    Update diferencias.py

--- diferencias.py
+++ diferencias.py
@@ -3,4 +3,8 @@ import matplotlib.pyplot as plt
 import scipy as spy # optimización y ajuste de curvas, procesamiento de imagenes, AL avanzada, estadística, integración y EDOs.
 from skimage import io # procesamiento de imágenes digitales. 
 
-  
+salon = io.imread('imagen_salon.png')
+
+print(salon.shape)
+plt.imshow(salon)
+
