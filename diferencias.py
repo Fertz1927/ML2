8a0697ba8fc6a8fa94036b85906a7859b53b6e8e
Fertz1927 <161084824+Fertz1927@users.noreply.github.com>
2026-09-12 13:05:26 -0500

    Create diferencias.py

---
+++ diferencias.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as spy # optimización y ajuste de curvas, procesamiento de imagenes, AL avanzada, estadística, integración y EDOs.
+from skimage import io # procesamiento de imágenes digitales. 
+
+  
