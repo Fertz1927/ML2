9e6a9370f38d727743e44fa696a645e9b93e5bc5
juan9d <juan.garcia.amaya@correounivalle.edu.co>
2026-09-13 12:57:55 -0500

    holy shit punto 2

--- diferencias.py
+++ diferencias.py
@@ -1,11 +1,24 @@
 import cv2
 import numpy as np
+from scipy.ndimage import convolve
 
-# primer punto
-M1 = cv2.imread('left.png').astype(np.int16)
-M2 = cv2.imread('right.png').astype(np.int16)
+M1 = cv2.imread('left.png')
+M2 = cv2.imread('right.png')
 
-diff = np.abs(M1 - M2)
-diff = diff.astype(np.uint8)
+def primerpunto():
+    diff = np.abs(M1.astype(np.int16) - M2.astype(np.int16))
+    diff = diff.astype(np.uint8)
 
-cv2.imwrite('diferencia.png', diff)
+    cv2.imwrite('diferencia.png', diff)
+
+def segundopunto():
+    for i in range(1,6):
+        kernel = (cv2.imread(f'Object-{i}.png').astype(np.float32))
+        resultado = convolve(M1.astype(np.float32), kernel)
+        resultado = resultado/np.max(resultado) * 255
+        resultado = resultado.astype(np.uint8)
+
+        cv2.imwrite(f'diferencia{i}.png', resultado)
+        print(f'objeto {i} hecho!11!')
+
+segundopunto()
