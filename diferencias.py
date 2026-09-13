d478ce7f6eb74e88382801a1a87454b9cef6ccf8
juan9d <juan.garcia.amaya@correounivalle.edu.co>
2026-09-13 11:56:42 -0500

    fix

--- diferencias.py
+++ diferencias.py
@@ -2,12 +2,10 @@ import cv2
 import numpy as np
 
 # primer punto
-M1 = cv2.imread('left.png')
-M2 = cv2.imread('right.png')
+M1 = cv2.imread('left.png').astype(np.int16)
+M2 = cv2.imread('right.png').astype(np.int16)
 
-diff = np.abs(np.round(M1/2) - np.round(M2/2))
+diff = np.abs(M1 - M2)
 diff = diff.astype(np.uint8)
 
 cv2.imwrite('diferencia.png', diff)
-
-# segundo punto
